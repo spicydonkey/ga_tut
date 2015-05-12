dbc36d2552587a7c2e730c4db5acea68547ae5e1
spicydonkey <dk.shin1992@gmail.com>
2015-05-12 12:02:28 +1200

    Create GATESTER.h
    a header file with Schwefel function to test a GA

---
+++ GATESTER.h
@@ -0,0 +1,12 @@
+// A library of Mathematical functions to test a genetic algorithm
+// <cmath> library must be imported
+
+float schwefel (float *argv, int dim)
+{
+	float value = 418.9829f*dim;
+	for (int i=0; i<dim; i++)
+	{
+		value -= argv[i]*sin(sqrt(fabs(argv[i])));
+	}
+	return value;
+}
